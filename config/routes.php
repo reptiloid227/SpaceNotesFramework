adb34205e0a94699c591c51b5c80cc47ae5ebf5c
reptiloid227 <alex.reptiloid227@gmail.com>
2023-05-28 00:10:43 +0500

    Update Router

--- config/routes.php
+++ config/routes.php
@@ -6,6 +6,6 @@ Router::add('^admin/?$', ['controller' => 'Main', 'action' => 'index', 'admin_pr
 
 Router::add('^admin/(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$', ['admin_prefix' => 'admin']);
 
-Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
+Router::add('^$', ['controller' => 'Main']);
 
 Router::add('^(?P<controller>[a-z-]+)/(?P<action>[a-z-]+)/?$');
