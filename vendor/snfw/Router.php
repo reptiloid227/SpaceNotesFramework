81401c79f6b5a12f9f7c860bcf2ae43659717b37
reptiloid227 <alex.reptiloid227@gmail.com>
2023-05-28 01:01:23 +0500

    Remove query string

--- vendor/snfw/Router.php
+++ vendor/snfw/Router.php
@@ -22,8 +22,20 @@ class Router
         return self::$route;
     }
 
+    protected static function removeQueryString($url)
+    {
+        if($url){
+            $params = explode("&", $url, 2);
+            if(false === str_contains($params[0], "=")){
+                return rtrim($params[0], '/');
+            }
+        }
+        return '';
+    }
+
     public static function dispatch($url)
     {
+        $url = self::removeQueryString($url);
         if (self::matchRoute($url)){
             $controller = 'App\controllers\\' . self::$route['admin_prefix'] . self::$route['controller'] . 'Controller';
             if(class_exists($controller)){
